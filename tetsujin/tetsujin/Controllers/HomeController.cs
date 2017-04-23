07f9d1c19bb3496ad286a2bb3eb282fe87f9b429
hMatoba <hiroaki.mtb@outlook.com>
2017-04-23 23:51:47 +0900

    ログインページの追加（機能未実装）

--- tetsujin/tetsujin/Controllers/HomeController.cs
+++ tetsujin/tetsujin/Controllers/HomeController.cs
@@ -6,13 +6,40 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace tetsujin.Controllers
 {
+    [Route("")]
     public class HomeController : Controller
     {
+        [Route("")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+        [Route("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public RedirectResult LoginAuth()
+        {
+            var loginPassed = false;
+
+            if (loginPassed)
+            {
+                return Redirect("/Master");
+            }
+            else
+            {
+                return Redirect("/Login");
+            }
+        }
+
+
+        [Route("{*path}")]
         public IActionResult Error()
         {
             return View();
