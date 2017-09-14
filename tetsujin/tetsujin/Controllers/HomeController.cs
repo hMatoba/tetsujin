69f46f53e042fde591b9c341a11eba52f2b6503e
hMatoba <hiroaki.mtb@outlook.com>
2017-09-15 08:44:00 +0900

    コントローラの初期化

--- tetsujin/tetsujin/Controllers/HomeController.cs
+++ tetsujin/tetsujin/Controllers/HomeController.cs
@@ -10,22 +10,9 @@ namespace tetsujin.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("")]
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
