01cb79a3cfb877765e6554bdb1e5b28c2ae1c117
hMatoba <hiroaki.mtb@outlook.com>
2017-09-17 22:34:15 +0900

    管理者用ページの追加（ログイン、認証はまだ）

---
+++ tetsujin/tetsujin/Controllers/MasterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using tetsujin.Models;
+
+namespace tetsujin.Controllers
+{
+    [Route("Master")]
+    public class MasterController : Controller
+    {
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
