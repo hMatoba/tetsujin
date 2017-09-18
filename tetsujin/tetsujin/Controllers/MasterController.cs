3cc6a7098a2701185f39f52c08d20488c535c4d5
hMatoba <hiroaki.mtb@outlook.com>
2017-09-18 12:02:07 +0900

    記事追加、編集機能作成

--- tetsujin/tetsujin/Controllers/MasterController.cs
+++ tetsujin/tetsujin/Controllers/MasterController.cs
@@ -19,5 +19,36 @@ namespace tetsujin.Controllers
             return View();
         }
 
+        [Route("Edit/{id:int?}")]
+        public IActionResult Edit(int? id)
+        {
+            Entry entry;
+            if (id != null)
+            {
+                int entryId = id ?? 0;
+                entry = Entry.GetEntry(entryId, true);
+            }
+            else
+            {
+                var timedelta = 9;
+                entry = new Entry
+                {
+                    Tag = new List<string> { },
+                    PublishDate = DateTime.Now.AddHours(timedelta),
+                    IsShown = true
+                };
+            }
+            ViewBag.entry = entry;
+            return View(entry);
+        }
+
+        [Route("Edit")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditPost(Entry entry)
+        {
+            entry.InsertOrUpdate();
+            return View();
+        }
     }
 }
---
+++ tetsujin/tetsujin/Models/Entry.cs
@@ -0,0 +1,122 @@
+﻿using MangoFramework;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace tetsujin.Models
+{
+    [MongoDoc]
+    public class Entry
+    {
+        public static string CollectionName = "Entry";
+
+        [BsonId]
+        [BsonElement("_id")]
+        [BsonRepresentation(BsonType.Int32)]
+        [HiddenInput]
+        public int? EntryID { get; set; } = null;
+
+        [DisplayName("タイトル")]
+        [BsonRepresentation(BsonType.String)]
+        [BsonElement("title")]
+        public string Title { get; set; }
+
+        [DisplayName("発行日時")]
+        [BsonRepresentation(BsonType.DateTime)]
+        [BsonElement("publishDate")]
+        [DataType(DataType.DateTime)]
+        public DateTime PublishDate { get; set; }
+
+        [BsonRepresentation(BsonType.String)]
+        [BsonElement("tag")]
+        public List<string> Tag { get; set; }
+
+        [DisplayName("タグ")]
+        [BsonIgnore]
+        public string _Tag { get; set; }
+
+        [DisplayName("本文")]
+        [BsonRepresentation(BsonType.String)]
+        [BsonElement("body")]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+
+        [DisplayName("掲載フラグ")]
+        [BsonRepresentation(BsonType.Boolean)]
+        [BsonElement("isShown")]
+        public bool IsShown { get; set; }
+
+        public static Entry GetEntry(int id, bool admin = false)
+        {
+            var collection = DbConnection.Db.GetCollection<Entry>(Entry.CollectionName);
+
+            FilterDefinition<Entry> filter;
+            var f = Builders<Entry>.Filter;
+            if (admin)
+            {
+                filter = f.Eq(e => e.EntryID, id);
+            }
+            else
+            {
+
+                filter = f.Eq(e => e.EntryID, id) &
+                         f.Eq(e => e.IsShown, true);
+            }
+
+            var entry = collection.Find<Entry>(filter).FirstOrDefault();
+            if (entry != null && entry.Tag.Count() > 0)
+            {
+                entry._Tag = String.Join(",", entry.Tag);
+            }
+
+            return entry;
+        }
+
+        public void InsertOrUpdate()
+        {
+            this.Tag = !String.IsNullOrEmpty(this._Tag) ? this._Tag.Split(',').ToList<string>() : new List<string> { };
+
+            if (EntryID == null)
+            {
+                Insert();
+            }
+            else
+            {
+                Update();
+            }
+        }
+
+        public void Insert()
+        {
+            var collection = DbConnection.Db.GetCollection<Entry>(Entry.CollectionName);
+
+            var sortDoc = Builders<Entry>.Sort.Descending(e => e.EntryID);
+            var d = collection.Find<Entry>(new BsonDocument { })
+                              .Sort(sortDoc)
+                              .FirstOrDefault<Entry>();
+
+            var id = (d == null) ? 0 : d.EntryID + 1;
+
+            this.EntryID = (int)id;
+            collection.InsertOne(this);
+        }
+
+        public void Update()
+        {
+            Tag.RemoveAll(item => item == null);
+
+            var collection = DbConnection.Db.GetCollection<Entry>(Entry.CollectionName);
+
+            var filter = Builders<Entry>.Filter.Eq("_id", this.EntryID);
+            collection.ReplaceOne(filter, this);
+        }
+
+    }
+
+}
