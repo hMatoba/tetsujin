2b0b1810ae81e6837f32472204f27efac5e99d31
hMatoba <hiroaki.mtb@outlook.com>
2017-09-22 07:31:56 +0900

    ユニットテストプロジェクトを追加

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using tetsujin;
+
+namespace UnitTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
