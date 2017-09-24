2f5590d03958d8027a3e61b745b290b65ad6e6e7
hMatoba <hiroaki.mtb@outlook.com>
2017-09-24 22:47:02 +0900

    masterへのコミット時のみデプロイを行うように修正

---
+++ tetsujin/deploy.sh
@@ -0,0 +1,15 @@
+# !/bin/sh
+if test "$TRAVIS_BRANCH" = "master"; then
+  docker tag matoba/tetsujin:ci matoba/tetsujin:latest
+  docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+  docker push matoba/tetsujin:latest
+  wget https://hyper-install.s3.amazonaws.com/hyper-linux-x86_64.tar.gz
+  tar xzf hyper-linux-x86_64.tar.gz
+  mv -f docker-compose.hyper.yml docker-compose.yml
+  cat docker-compose.yml
+  ./hyper config --accesskey ${HYPER_ACCESS} --secretkey ${HYPER_SECRET} --default-region "us-west-1"
+  ./hyper compose down --rmi -p cdcc
+  ./hyper compose up -d -p cdcc
+else
+  echo "$TRAVIS_BRANCH branch"
+fi
