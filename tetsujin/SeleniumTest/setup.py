ce3de1f777cea1fc2acfbdebf64473596647c21d
hMatoba <hiroaki.mtb@outlook.com>
2017-10-24 23:39:01 +0900

    ファイル名変更

--- tetsujin/SeleniumTest/setup.py
+++ tetsujin/SeleniumTest/setup.py
@@ -7,7 +7,7 @@ setup(
     name='browsertest',
     version='1.0',
     description='test a project on browser',
-    test_suite = 's_test.suite',
+    test_suite = 'selenium_test.suite',
     install_requires=[
         'selenium',
         'pymongo'
