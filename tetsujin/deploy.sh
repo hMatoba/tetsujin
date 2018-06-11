6343a921ed60af678041424e3354e8003f33c480
hMatoba <hiroaki.mtb@outlook.com>
2018-06-11 22:18:10 +0900

    開発中コンテナのタグの変更

--- tetsujin/deploy.sh
+++ tetsujin/deploy.sh
@@ -1,6 +1,6 @@
 # !/bin/sh -x
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  docker tag matoba/tetsujin:ci matoba/tetsujin:latest
+  docker tag matoba/tetsujin:dev matoba/tetsujin:latest
   docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
   docker push matoba/tetsujin:latest
   docker tag matoba/tetsujin:latest matoba/tetsujin:${TRAVIS_BUILD_NUMBER}
